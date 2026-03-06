2f75f42e19200a93b4a8fcd4cc42d6387b5c5ea6
Kris <Kris.005Kouame.2024@gmail>
2026-03-06 16:37:59 +0000

    code stable

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.kbboutik_v04"
+    //ndkVersion = "27.0.12077973"
     compileSdk = 36
 
     defaultConfig {
@@ -49,12 +50,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
 
     // 🔹 Core library desugaring
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
-    // 🔹 Firebase BOM
-    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
 
-    // 🔹 Firebase KTX libs
-    implementation("com.google.firebase:firebase-messaging-ktx")
-    implementation("com.google.firebase:firebase-analytics-ktx")
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'firebase_options.dart'; // généré par flutterfire configure
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
